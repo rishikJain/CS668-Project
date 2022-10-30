add3f0934435a43bf22720d4ff74510014b636c0
blackbuck <apple@Apples-MacBook-Pro.local>
2022-10-30 09:09:04 +0530

    update model

--- backend/models/model.js
+++ backend/models/model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const dataSchema = new mongoose.Schema({
     asset: {
         required: true,
-        type: String
+        type: [String]
     },
     deviceType: {
         required: true,
